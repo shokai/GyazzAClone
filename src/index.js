d43f7f4f9b838e4df952f33a889ce54863cd43c6
Sho Hashimoto <hashimoto@shokai.org>
2016-07-02 15:40:39 +0900

    getLines

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-alert('start')
+import $ from 'jquery'
+
+console.log('Gyazz(A)Clone start')
+
+convertLinesToSlide()
+
+function convertLinesToSlide () {
+  const lines = getLines()
+  console.log(lines)
+}
+
+function getLines () {
+  let lines = $('.lines .line')
+  console.log(`${lines.length} liens found`)
+  const result = []
+  for (let index = 0; index < lines.length; index++) {
+    let line = lines[index]
+    let $_ = $(line)
+    let text = $_.text()
+    let indent = text.match(/^(\s*)/)[0].length
+    result.push({ dom: line, text, indent, index })
+  }
+  return result
+}
